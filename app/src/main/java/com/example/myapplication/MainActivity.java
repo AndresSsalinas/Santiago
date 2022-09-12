8cd42df9db4a3cc226d3cc7dfba12571f258241f
AndresSsalinas <cristhian.salinasd@uniagustiniana.edu.co>
2022-09-11 23:39:06 -0500

    All

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,90 +8,73 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_FAc;
     private Button btn_FI;
 
     private TextView text_respuesta;
+    private TextView text_respuesta2;
 
     private EditText edit_numero_uno;
-    private EditText edit_numero_dos;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        text_respuesta = findViewById(R.id.respuesta);
+        text_respuesta = findViewById(R.id.Fibonacci);
+        text_respuesta2 = findViewById(R.id.Factorial);
 
-        edit_numero_uno = findViewById(R.id.numero1);
-        edit_numero_dos = findViewById(R.id.numero2);
+        edit_numero_uno = findViewById(R.id.Dato);
 
-
-
-
-
-
-        btn_suma = findViewById(R.id.button_suma);
-        btn_suma.setOnClickListener(new View.OnClickListener() {
+        btn_FI = findViewById(R.id.buttonFib);
+        btn_FI.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                text_respuesta.setText( suma( Integer.parseInt(edit_numero_uno.getText().toString()),Integer.parseInt(edit_numero_dos.getText().toString()) )+"");
+                text_respuesta.setText( fibo( Integer.parseInt(edit_numero_uno.getText().toString()))+"");
             }
         });
 
-
-        btn_division = findViewById(R.id.button_division);
-        btn_division.setOnClickListener(new View.OnClickListener() {
+        btn_FAc = findViewById(R.id.buttonFac);
+        btn_FAc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                text_respuesta.setText( division( Integer.parseInt(edit_numero_uno.getText().toString()),Integer.parseInt(edit_numero_dos.getText().toString()) )+"");
-
-            }
-        });
-
-        btn_multiplicacion = findViewById(R.id.button_multiplicacion);
-        btn_multiplicacion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                text_respuesta.setText( multiplicacion( Integer.parseInt(edit_numero_uno.getText().toString()),Integer.parseInt(edit_numero_dos.getText().toString()) )+"");
-
-            }
-        });
-
-        btn_resta = findViewById(R.id.button_resta);
-        btn_resta.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                text_respuesta.setText( resta( Integer.parseInt(edit_numero_uno.getText().toString()),Integer.parseInt(edit_numero_dos.getText().toString()) )+"");
+                text_respuesta2.setText( Fac( Integer.parseInt(edit_numero_uno.getText().toString()))+"");
 
             }
         });
 
     }
 
-    public double suma (int a, int b){
-        return a+b;
-    }
-
-    public double resta (int a, int b){
-        return a-b;
-    }
-
-    public double multiplicacion (int a, int b){
-        return a*b;
-    }
-
-    public double division (int a, int b){
-        int respuesta = 0;
-
-        if (b!=0){
-            respuesta=a/b;
+    public String fibo (int a){
+        String text ="";
+        ArrayList<BigInteger> fib = new ArrayList<>();
+        BigInteger first = new BigInteger("0");
+        BigInteger second = new BigInteger("1");
+        fib.add(first);
+        fib.add(second);
+        for(int i = 1;i<a;i++){
+            fib.add(fib.get(i).add(fib.get(i-1)));
         }
 
-        return respuesta;
+        for(int i = 1;i<=a;i++){
+           text = text + i + ". " + fib.get(i) + "\n";
+        }
+        return text;
+    }
+
+    public String Fac (int a){
+        int factorial = 1;
+        String text ="";
+
+        for (int i=a;i>0;i--) {
+            factorial=factorial*i;
+        }
+        text= ""+ factorial;
+        return text;
     }
 }
